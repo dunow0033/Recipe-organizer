9ed607f4001e225d658fc8d1bbfe349a3de7efc9
dunow <dunow33@hotmail.com>
2021-05-14 23:26:37 -0400

    added serializers, pry, install redux packages on thr front

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index] do
+    resources :ingredients, only: [:index]
+  end
 end
