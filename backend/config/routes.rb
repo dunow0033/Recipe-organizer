0e144c2e4abd550b761810e30398d4473407d6ae
dunow <dunow33@hotmail.com>
2021-05-18 16:04:49 -0400

    updating post and delete routes on the back end

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index] do
-    resources :ingredients, only: [:index]
+  resources :recipes, only: [:index, :create, :delete] do
+    resources :ingredients, only: [:index, :create, :delete]
   end
 end
