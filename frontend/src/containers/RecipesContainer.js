76e0a8df2c3fa888b06b23c2009484d94a8aa9b6
dunow <dunow33@hotmail.com>
2021-05-21 17:23:40 -0400

    more work on adding ingredients

--- frontend/src/containers/RecipesContainer.js
+++ frontend/src/containers/RecipesContainer.js
@@ -18,7 +18,7 @@ class RecipesContainer extends React.Component {
             <div>
                 <Switch>
                     <Route exact path='/recipes/new' component={RecipeInput} />
-                    <Route exact path='/recipes/:id' render={() => <IngredientsInput recipe={this.props.recipes} />}
+                    <Route exact path='/recipes/:id' render={() => <IngredientsInput recipe={this.props.recipes} />}/>
                     <Link to="/recipes/new">Make a New Recipe</Link>
                     {/*<Route path='/recipes/:id' render={(routerProps) => <Recipe {...routerProps} recipes={this.props.recipes}/>}/>*/}
                 </Switch>
