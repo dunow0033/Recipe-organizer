3c3d3fc2f88b491a02812118d8398b3619aca037
dunow <dunow33@hotmail.com>
2021-06-03 15:44:26 -0400

    just noticed the delete instruction reducer had been commented out, wanted to re add it and test it to make sure it worked.

--- frontend/src/reducers/recipeReducer.js
+++ frontend/src/reducers/recipeReducer.js
@@ -4,8 +4,6 @@ export default function recipeReducer(state = {recipes: []}, action) {
             return {recipes: action.payload};
         case 'ADD_RECIPE':
             return {...state, recipes: [...state.recipes, action.payload]}
-        case 'DELETE_RECIPE':
-            return state.recipes.filter(recipe => recipe.id === action.payload.id)
         case 'ADD_INGREDIENT':
             let recipes = state.recipes.map(recipe => {
                 if(recipe.id === action.payload.id){
@@ -33,15 +31,15 @@ export default function recipeReducer(state = {recipes: []}, action) {
                 }
             })
             return {...state, recipes: recipes3}
-        // case 'DELETE_INSTRUCTION':
-        //         let recipes4 = state.recipes.map(recipe => {
-        //             if(recipe.id === action.payload.id){
-        //                 return action.payload
-        //             } else {
-        //                 return recipe
-        //             }
-        //         })
-        //         return {...state, recipes: recipes4}
+        case 'DELETE_INSTRUCTION':
+                let recipes4 = state.recipes.map(recipe => {
+                    if(recipe.id === action.payload.id){
+                        return action.payload
+                    } else {
+                        return recipe
+                    }
+                })
+                return {...state, recipes: recipes4}
         default:
             return state
     }
